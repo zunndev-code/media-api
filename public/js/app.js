55c4086658df4004444abe8b91d2d325fe90ac53
zunndev-code <oocah568@gmail.com>
2026-08-19 09:11:23 +0000

    email verification: i18n reg.* keys (EN+ID), separate email line, verify UI complete

--- public/js/app.js
+++ public/js/app.js
@@ -56,7 +56,7 @@ function navLink(l, cls) {
     '<span data-i18n="' + l.label + '">' + tr(l.label) + '</span></a>';
 }
 
-const LOGO_MARK = '<img class="logo-img" src="/img/logo.png?v=81" alt="Ziplan">';
+const LOGO_MARK = '<img class="logo-img" src="/img/logo.png?v=82" alt="Ziplan">';
 
 function buildNav() {
   const nav = $('nav');
